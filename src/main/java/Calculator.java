c69b97d033c0fc9bc08bb0adde1e647d1d1a1c25
TheLutik <denistudent07@gmail.com>
2022-11-04 14:13:07 +0300

    2nd commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public double mul(int a, int b) {
+        return (a * b);
+    }
+
+
+    public double divide(double a, double b) {
+        return (a / b);
+    }
+}
